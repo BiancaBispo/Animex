0268d92c6c9903ff4dcc10c1d3a10c6148651fcd
Bianca Bispo <biancabss12@gmail.com>
2022-09-21 17:22:06 -0300

    import Service

--- src/app/service/animex-api.service.ts
+++ src/app/service/animex-api.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
 
+// Importar o model
+import {Anime} from '../model/anime';
+
+//Importar os recursos para lidar com as requisições http
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError,  } from 'rxjs';
+import { retry, catchError } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
