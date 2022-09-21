bcacebaaf767ace2ecac85ba6d9bd552901b6ffb
rafa <ralfrr55@gmail.com>
2022-09-21 16:22:16 -0300

    criacao de componente model service2...

---
+++ src/app/componentes/anime-create/anime-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anime-create',
+  templateUrl: './anime-create.component.html',
+  styleUrls: ['./anime-create.component.css']
+})
+export class AnimeCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
