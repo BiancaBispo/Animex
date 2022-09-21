d06f89c0f425d0bf034022caa3dd8708c9bfbf87
Bianca Bispo <biancabss12@gmail.com>
2022-09-21 17:26:48 -0300

    import Service

--- src/app/service/animex-api.service.ts
+++ src/app/service/animex-api.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 
 // Importar o model
-import {Anime} from '../model/anime';
+import { Anime } from '../model/anime';
 
 //Importar os recursos para lidar com as requisições http
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError,  } from 'rxjs';
-import { retry, catchError } from 'rxjs';
+import {  HttpClient, HttpHeaders  } from '@angular/common/http';
+import {  Observable, throwError  } from 'rxjs';
+import {  retry, catchError  } from 'rxjs';
 
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+
 export class AnimexApiService {
 
   constructor() { }
