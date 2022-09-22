bfb6e982c53d354900e14b0bfae2d64d2efe5e3a
Bianca Bispo <biancabss12@gmail.com>
2022-09-22 10:08:19 -0300

    editar

--- src/app/componentes/anime-edit/anime-edit.component.ts
+++ src/app/componentes/anime-edit/anime-edit.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+//Importar o service
+import { AnimexApiService } from 'src/app/service/animex-api.service';
+
+//Importar a classe Router 
+import { ActivatedRoute, Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-anime-edit',
   templateUrl: './anime-edit.component.html',
@@ -7,9 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimeEditComponent implements OnInit {
 
-  constructor() { }
+  // Titulo ao componente
+  tituloComp: string = 'Alterar Anime'
 
+  constructor(
+    public animexApi: AnimexApiService,
+    public roteamento: Router,
+    public gps: ActivatedRoute
+  ) { }
+
+  // Copia da rota
+  copiaRota = this.gps.snapshot.params['id']
+
+  //Receber os dados
+  atualizarAnime: any = {} 
+
+  //Priorizar o carregamento
   ngOnInit(): void {
+    this.animexApi.acessarUmAnime(this.copiaRota).subscribe((dados:any) => {
+      this.atualizarAnime = dados
+    })
   }
+
+  // Atualizar
+  atualizacaoAnimes(){
+    if(confirm('Tem certeza que deseja alterar esse Anime?')){
+      this.animexApi.alterarAnime(this.copiaRota, this.atualizarAnime).subscribe(() => {
+        this.roteamento.navigate(['/anime-list']) //Voltar para lista quando finalizado
+      })
+    }
+  }
+
 
 }
